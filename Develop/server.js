7dd412d7d6bac53cf92a08f59054d9110ec0316c
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2023-11-10 16:05:20 -0500

    Made a working note taker, Post and Get requests are Working Perfectly

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 const path = require("path")
 const app = express();
 
@@ -12,7 +14,34 @@ app.get("/",(req,res) => {
 })
 
 app.get("/notes",(req,res) => {
+    console.log(`${req.method} was the received method`)
     res.sendFile(path.join(__dirname,"public/notes.html"))
+    
+})
+app.get("/api/notes",(req,res) => {
+    fs.readFile("./db/db.json","utf8",(err,data)=> {
+        if(err)throw err
+        res.send(data);
+    })
+})
+app.post("/api/notes",(req,res) => {
+   const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4()
+   }
+   console.log(newNote)
+   fs.readFile("./db/db.json","utf8",(err,data)=>{
+    if(err)throw err
+    const noteData = JSON.parse(data)
+    noteData.push(newNote)
+    console.log(noteData)
+
+    fs.writeFile("./db/db.json",JSON.stringify(noteData),(err,data)=>{
+        if(err)throw err;
+        res.json(noteData)
+    })
+   })
 })
 
 
