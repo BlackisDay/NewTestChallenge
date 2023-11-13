e8248d551ea6e35b3cc543cfa18eedc6ff873663
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2023-11-13 17:59:41 -0500

    Updated file location and port code

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 const path = require("path")
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT||3001;
 app.use(express.static("public"))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,"./index.html"))
+    res.sendFile(path.join(__dirname,"public/index.html"))
 
 })
 
